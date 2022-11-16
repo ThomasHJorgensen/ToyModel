fe7b292199ff1c21c1a117d251a0d957b99d7130
ThomasHJorgensen <tjo@econ.ku.dk>
2022-11-16 17:28:45 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# Readme
+
+This readme describes how to install everything required for running the `Solve Toy Model.ipynb` notebook before describing how to execute the code and how the code base is structured.
+
+# Installation
+1. Install Python: E.g. the free [Anaconda](https://www.anaconda.com/products/distribution)
+2. Install packages
+    1. open Anaconda Prompt (e.g. by searching)
+    2. type `pip install EconModel, consav`
+
+These two packages are developed and maintained by Jeppe Druedahl for the purpose of easy implementaion of efficient (Numba and C/C++) code to solve consumption-savings models. See [consav package](https://github.com/NumEconCopenhagen/ConsumptionSaving) and [EconModel package](https://github.com/NumEconCopenhagen/EconModel) for more information.
+
+# Compilation
+The code is implemented in C++. This means that the code should be re-compiled if anything is changed. The `Solve Toy Model.ipynb` notebook does this automatically if requested (`do_compile=True`). To compile the files, Visual Studio 2022 should be installed in the path `C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build/`.
+
+The Visual Studio compiler is free and can be downloaded for free here: [Visual Studio](https://visualstudio.microsoft.com/vs/whatsnew/).
+
+# Execution
+The code is executed from a **Jupyter Lab** notebook called `Solve Toy Model.ipynb`. This notebook executes code and illustrates results.
+
+1. open jupyter lab by executing the `open_jupyter.bat` file. This opens up the browser. Chrome is preferred, which you can dowload [here](https://www.google.com/chrome/).
+2. run the notebook. To execute the entire notebook you can press the the play button in the top. To execute individual cells (and advance to the next cell) press `shift+enter`. You can read more about the Jupyter Lab interface [here](https://jupyterlab.readthedocs.io/en/stable/user/interface.html).
+
+# Code
+The main Python code is contained in **ToyModel.py** which calls C++ code. 
+
+**Python:** In Python the model instance (object) is initiated, memory is allocated and parameters and grids are set. Building on the `EconModel`, Python also handles all convertions to C++ that we will utilize below. This means that all parameters and grids should be set in `ToyModel.py`. The main C++ file that Python should know about is set in `cpp_filename = 'cppfuncs/solve.cpp'`.
+
+**C++:** All the code solving and simulating the model is implemented in C++ for speed. The `.cpp` files are all stored in the `cppfuncs` folder, which the `EconModel` class assumes. All functions that should be callable from Python must have the `EXPORT` decorator in front of the function definition. All functions will then be callable from Python using `model.cpp.functionname`. See e.g. the function `solve` in `ToyModel.py`. In this example, calling `model.solve()` will automatically call the C++ implementaion. See also [EconModelNotebooks](https://github.com/NumEconCopenhagen/EconModelNotebooks) for illustrations of the link to C++.
---
+++ ToyModel.py
@@ -0,0 +1,220 @@
+import numpy as np
+import numba as nb
+import scipy.optimize as optimize
+
+from EconModel import EconModelClass
+from consav.grids import nonlinspace
+from consav import linear_interp, linear_interp_1d
+from consav import quadrature
+
+# set gender indication as globals
+woman = 1
+man = 2
+
+class HouseholdModelClass(EconModelClass):
+    
+    def settings(self):
+        """ fundamental settings """
+
+        # a. namespaces
+        self.namespaces = []
+        
+        # b. other attributes
+        self.other_attrs = []
+        
+        # c. savefolder
+        self.savefolder = 'saved'
+        
+        # d. cpp
+        self.cpp_filename = 'cppfuncs/solve.cpp'
+        self.cpp_options = {'compiler':'vs'}
+        
+    def setup(self):
+        par = self.par
+        
+        par.R = 1.03
+        par.beta = 1.0/par.R # Discount factor
+        
+        # divorce
+        par.div_A_share = 0.5 # divorce share of wealth to wife
+        par.div_H_share =  1.0 # how much of the home capital survives a divorce
+        
+        # wages
+        par.wage_const = 0.0
+        par.wage_K = 0.05
+
+        # Utility: 
+        par.rho = 2.0 
+
+        # home production
+        par.home_power_1 = -0.1
+        par.home_power_2 = 0.5
+        par.home_load_1 = 1.0
+        par.home_load_2 = 1.0   
+
+        # state accumulation
+        par.depre_K = 0.1
+        par.depre_H = 0.1
+
+        par.accum_K = 1.0
+        par.accum_H = 1.0 
+        
+        # state variables
+        par.T = 3
+        par.max_time = 1.0
+        
+        # wealth
+        par.num_A = 50
+        par.max_A = 15.0
+
+        # human capital
+        par.num_K = 10
+        par.max_K = 10.0
+
+        # home capital
+        par.num_H = 15
+        par.max_H = 10.0        
+        
+        # bargaining power
+        par.num_power = 21
+
+        # love/match quality
+        par.num_love = 41
+        par.max_love = 1.0
+
+        par.sigma_love = 0.1
+        par.num_shock_love = 5
+
+        # simulation
+        par.seed = 9210
+        par.simT = par.T
+        par.simN = 50_000
+
+        # cpp
+        par.threads = 8
+
+        # pre-computation
+        par.num_pre_h = 50
+        par.num_pre_C = 100
+        
+    def allocate(self):
+        par = self.par
+        sol = self.sol
+        sim = self.sim
+
+        # setup grids
+        self.setup_grids()
+        
+        # singles
+        shape_single = (par.T,par.num_H,par.num_K,par.num_A)
+        sol.V_w_single = np.nan + np.ones(shape_single)
+        sol.V_m_single = np.nan + np.ones(shape_single)
+        
+        sol.c_w_single = np.nan + np.ones(shape_single)
+        sol.l_w_single = np.nan + np.ones(shape_single)
+        sol.h_w_single = np.nan + np.ones(shape_single)
+        sol.m_w_single = np.nan + np.ones(shape_single)
+        
+        sol.c_m_single = np.nan + np.ones(shape_single)
+        sol.l_m_single = np.nan + np.ones(shape_single)
+        sol.h_m_single = np.nan + np.ones(shape_single)
+        sol.m_m_single = np.nan + np.ones(shape_single)
+
+        # pre-computation
+        shape_pre_single = (par.num_H,par.num_pre_h,par.num_pre_C)
+        sol.pre_cons_w_single = np.nan + np.ones(shape_pre_single)
+        sol.pre_market_w_single = np.nan + np.ones(shape_pre_single)
+        sol.pre_cons_m_single = np.nan + np.ones(shape_pre_single)
+        sol.pre_market_m_single = np.nan + np.ones(shape_pre_single)
+                            
+
+        # couples
+        # shape_couple = (par.T,par.num_power,par.num_love,par.num_A)
+        # sol.Vw_couple = np.nan + np.ones(shape_couple)
+        # sol.Vm_couple = np.nan + np.ones(shape_couple)
+        
+        # sol.Cw_priv_couple = np.nan + np.ones(shape_couple)
+        # sol.Cm_priv_couple = np.nan + np.ones(shape_couple)
+        # sol.C_pub_couple = np.nan + np.ones(shape_couple)
+        # sol.C_tot_couple = np.nan + np.ones(shape_couple)
+
+        # sol.Vw_remain_couple = np.nan + np.ones(shape_couple)
+        # sol.Vm_remain_couple = np.nan + np.ones(shape_couple)
+        
+        # sol.Cw_priv_remain_couple = np.nan + np.ones(shape_couple)
+        # sol.Cm_priv_remain_couple = np.nan + np.ones(shape_couple)
+        # sol.C_pub_remain_couple = np.nan + np.ones(shape_couple)
+        # sol.C_tot_remain_couple = np.nan + np.ones(shape_couple)
+
+        # sol.power_idx = np.zeros(shape_couple,dtype=np.int_)
+        # sol.power = np.zeros(shape_couple)
+
+        
+    def setup_grids(self):
+        par = self.par
+        
+        # wealth. Single grids are such to avoid interpolation
+        par.grid_A = nonlinspace(1.0e-6,par.max_A,par.num_A,1.1)
+
+        par.grid_Aw = par.div_A_share * par.grid_A
+        par.grid_Am = (1.0 - par.div_A_share) * par.grid_A
+
+        # market human capital
+        par.grid_K = nonlinspace(0.0,par.max_K,par.num_K,1.1)
+
+        # home capital
+        par.grid_H = nonlinspace(0.0,par.max_H,par.num_H,1.1)
+
+        par.grid_Hw = par.div_H_share * par.grid_H
+        par.grid_Hm = par.div_H_share * par.grid_H
+
+        # power. non-linear grid with more mass in both tails.
+        odd_num = np.mod(par.num_power,2)
+        first_part = nonlinspace(0.0,0.5,(par.num_power+odd_num)//2,1.3)
+        last_part = np.flip(1.0 - nonlinspace(0.0,0.5,(par.num_power-odd_num)//2 + 1,1.3))[1:]
+        par.grid_power = np.append(first_part,last_part)
+
+        # love grid and shock
+        if par.num_love>1:
+            par.grid_love = np.linspace(-par.max_love,par.max_love,par.num_love)
+        else:
+            par.grid_love = np.array([0.0])
+
+        if par.sigma_love<=1.0e-6:
+            par.num_shock_love = 1
+            par.grid_shock_love,par.grid_weight_love = np.array([0.0]),np.array([1.0])
+
+        else:
+            par.grid_shock_love,par.grid_weight_love = quadrature.normal_gauss_hermite(par.sigma_love,par.num_shock_love)
+
+        # pre-computation
+        par.grid_pre_Ctot = np.linspace(par.grid_A[0],par.grid_A[-1],par.num_pre_C)
+        par.grid_pre_hours = np.linspace(0.0001,par.max_time,par.num_pre_h)
+
+    def solve(self):
+        sol = self.sol
+        par = self.par 
+
+        # setup grids
+        self.setup_grids()
+
+        # solve model
+        self.cpp.solve(sol,par)
+
+        
+
+    # def simulate(self):
+    #     sol = self.sol
+    #     sim = self.sim
+    #     par = self.par
+
+    #     if par.do_cpp:
+    #         self.cpp.simulate(sim,sol,par)
+
+    #     else:
+    #         simulate(sim,sol,par)
+
+    #     # total consumption
+    #     sim.Cw_tot = sim.Cw_priv + sim.Cw_pub
+    #     sim.Cm_tot = sim.Cm_priv + sim.Cm_pub
+    #     sim.C_tot = sim.Cw_priv + sim.Cm_priv + sim.Cw_pub
